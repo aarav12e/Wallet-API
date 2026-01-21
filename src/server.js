f7c7401bcb41f9b0f7e642e4374d9b3386df722c
aarav12e <aarav12f@gmail.com>
2026-01-21 06:56:01 +0530

    added cron

--- src/server.js
+++ src/server.js
@@ -3,10 +3,13 @@ import dotenv from "dotenv";
 import { initDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import transactionsRoute from "./routes/transactionsRoute.js";
+import cors from "./config/cors.js ";
 
 dotenv.config();
 
 const app = express();
+
+if (process.env.NODE_ENV === "production") job.start();
 
 //middleware
 app.use(rateLimiter);
@@ -14,6 +17,10 @@ app.use(rateLimiter);
 app.use(express.json());
 
 const PORT = process.env.PORT || 5001;
+
+app.get("/api/health", (req, res) => {
+    res.status(200).json({ status: "API is running" });
+});
 
 
 app.use("/api/transactions", transactionsRoute)
